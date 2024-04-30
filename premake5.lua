e01039f2decd4d5b78439638eea4cb4084439109
Mi11ie-a <66948271+Mi11ie-a@users.noreply.github.com>
2024-04-30 23:14:09 +1000

    forked

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "SPIRV-Cross"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"spirv_cross.cpp"
+	}
+
+    defines
+    {
+    }
+
+    includedirs
+    {
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter {"configurations:Dist"}
+        runtime "Release"
+        optimize "on"
+			
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
